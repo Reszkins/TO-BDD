5d14bd011fc5c964e2e9bfefb1bdcd5223331bd7
zvzia <gaik.zuza@gmail.com>
2022-12-06 19:45:12 +0100

    logowanie i rejestracja (nwm czy mokowanie dobre)

---
+++ TO-DBB-Tests/LoginTests.cs
@@ -0,0 +1,104 @@
+﻿using Moq;
+using TO_BDD.Models;
+using TO_BDD.Services;
+
+namespace TO_DBB_Tests
+{
+    public class LoginTests
+    {
+        [Fact]
+        public void Valid_Login_Data()
+        {
+            //Arrange
+            var userContextMock = new Mock<UserContext>();
+            userContextMock.Setup(x => x.User.Register(It.IsAny<User>())).Returns((User u) => u);
+            var userService = new UserService(userContextMock.Object);
+
+            string username = "user";
+            string password = "password";
+            userService.Register(username, password);
+
+            //Act
+            bool result = userService.Login(username, password);
+
+            //Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Invalid_Login_Data_Invalid_Username()
+        {
+            //Arrange
+            var userContextMock = new Mock<UserContext>();
+            userContextMock.Setup(x => x.User.Register(It.IsAny<User>())).Returns((User u) => u);
+            var userService = new UserService(userContextMock.Object);
+
+            string username = "user";
+            string password = "password";
+            userService.Register(username, password);
+
+            //Act
+            bool result = userService.Login("user1", password);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Invalid_Login_Data_Invalid_Password()
+        {
+            //Arrange
+            var userContextMock = new Mock<UserContext>();
+            userContextMock.Setup(x => x.User.Register(It.IsAny<User>())).Returns((User u) => u);
+            var userService = new UserService(userContextMock.Object);
+
+            string username = "user";
+            string password = "password";
+            userService.Register(username, password);
+
+            //Act
+            bool result = userService.Login(username, "password1");
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Invalid_Login_Data_Invalid_Blank_Username()
+        {
+            //Arrange
+            var userContextMock = new Mock<UserContext>();
+            userContextMock.Setup(x => x.User.Register(It.IsAny<User>())).Returns((User u) => u);
+            var userService = new UserService(userContextMock.Object);
+
+            string username = "user";
+            string password = "password";
+            userService.Register(username, password);
+
+            //Act
+            bool result = userService.Login("", password);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Invalid_Login_Data_Invalid_Blank_Password()
+        {
+            //Arrange
+            var userContextMock = new Mock<UserContext>();
+            userContextMock.Setup(x => x.User.Register(It.IsAny<User>())).Returns((User u) => u);
+            var userService = new UserService(userContextMock.Object);
+
+            string username = "user";
+            string password = "password";
+            userService.Register(username, password);
+
+            //Act
+            bool result = userService.Login(username, "");
+
+            //Assert
+            Assert.False(result);
+        }
+    }
+}
