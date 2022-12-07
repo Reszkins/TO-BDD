0d026feb0c7e8ec2d324d036ed30bc83e6be4516
Konrad Trusiewicz <konrad1.tr@gmail.com>
2022-12-07 16:24:26 +0100

    branch init, dbcontext

--- TO-BDD/Repositories/DbRepository.cs
+++ TO-BDD/Repositories/DbRepository.cs
@@ -1,7 +1,18 @@
-﻿namespace TO_BDD.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using TO_BDD.Models;
+
+namespace TO_BDD.Repositories
 {
-    public class DbRepository
+    public class DbRepository : DbContext
     {
-        //komunikacja z bazą danych
+        public DbRepository(DbContextOptions<DbRepository> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Cart> Users { get; set; }
     }
 }
