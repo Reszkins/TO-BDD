e1ee419175e1c4d9f7899435211bb1bfcd971ebf
zvzia <gaik.zuza@gmail.com>
2022-12-08 23:27:32 +0100

    BookComparer

--- TO-BDD/Models/Book.cs
+++ TO-BDD/Models/Book.cs
@@ -1,4 +1,5 @@
-﻿using TO_BDD.Enums;
+﻿using System;
+using TO_BDD.Enums;
 
 namespace TO_BDD.Models
 {
@@ -9,5 +10,37 @@ namespace TO_BDD.Models
         public string Description { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string Type { get; set; } = string.Empty;
+    }
+
+    public class BookComparer : Comparer<Book>
+    {
+        public override int Compare(Book x, Book y)
+        {
+            int val = x.Title.CompareTo(y.Title);
+            if (val != 0)
+            {
+                return val;
+            }
+
+            val = x.Description.CompareTo(y.Description);
+            if (val != 0)
+            {
+                return val;
+            }
+
+            val = x.Author.CompareTo(y.Author);
+            if (val != 0)
+            {
+                return val;
+            }
+
+            val = x.Type.CompareTo(y.Type);
+            if (val != 0)
+            {
+                return val;
+            }
+
+            return val;
+        }
     }
 }
